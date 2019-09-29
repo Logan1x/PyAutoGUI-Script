89c942bf300135606949c50d9a7d5fe3eb39f551
Khushal Sharma <sharmakhushal78@gmail.com>
2019-09-29 16:30:57 +0530

    Update PyAutoGUI.py

--- PyAutoGUI.py
+++ PyAutoGUI.py
@@ -3,13 +3,25 @@ import pyautogui,time
 a=int(input("Enter your choice : \n 1. Display mouse position \n 2. Click on position \n"))
 
 if(a==1):
-    pyautogui.displayMousePosition() 
+    pyautogui.displayMousePosition()
 
 else:
     pyautogui.FAILSAFE = True
-    time.sleep(3)
-    i=0
-    while(i<500):
-        pyautogui.click(x=1310, y=566)
-        i=i+1
-        print("Clicked " + str(i) + "th times")
+    print(" 1. press 1 if you want to input Value  \n 2. press 2 else")
+    b = int(input())
+    if(b==1):
+        print("your choice is 1 so enter value")
+        x = int(input("Enter X axis"))
+        y = int(input("Enter Y axis"))
+        time.sleep(3)
+        i=0
+        while(i<500):
+            pyautogui.click(x, y)
+            i=i+1
+            print("Clicked " + str(i) + "th times")
+    else:        
+        i=0
+        while(i<500):
+            pyautogui.click(x=1271, y=795)
+            i=i+1
+            print("Clicked " + str(i) + "th times")
